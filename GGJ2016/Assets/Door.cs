f4fc5b98640b688e11d388b9eb8b5f81ee202fc9
angel diaz <adiaz004@yahoo.com>
2016-01-30 08:30:34 -0700

    added a git ignore, updated next room.

---
+++ GGJ2016/Assets/Door.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+	public Transform startingPoint;
+	public Transform stoppingPoint;
+	public float speed;
+	bool OpenDoor = false;
+
+	void Update () {
+		float step = speed * Time.deltaTime;
+		if (OpenDoor) {
+			transform.position = Vector3.MoveTowards (transform.position, stoppingPoint.position, step);
+		} else {
+			transform.position = Vector3.MoveTowards (transform.position, startingPoint.position, step);
+		}
+	}
+
+	/// <summary>
+	/// Slides the door open.
+	/// </summary>
+	void Open () {
+		OpenDoor = true;
+	}
+
+	/// <summary>
+	/// Slides the door closes.
+	/// </summary>
+	void Close () {
+		OpenDoor = false;
+	}
+
+	void TurnOffLights (){
+	
+	}
+}
